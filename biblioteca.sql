0cdc82d547767303863ce766577f2e3f50613718
Sabrina Gomes <sabrina.gomess.dev@gmail.com>
2025-06-12 19:27:08 -0300

    feat: insert command membros, livros and autor tables

--- biblioteca.sql
+++ biblioteca.sql
@@ -53,9 +53,9 @@ INSERT INTO livros (id_autor, data_public, genero, titulo) VALUES
 
 INSERT INTO membros (data_nasc, nome, telefone) VALUES
 	('2012-06-20', 'Maria Silvana', '(11)96543-2131'),
-    ('1997-05-22', 'Gabriel Eduardo', '(99)94321-3432'),
-    ('1967-04-30', 'Roberto Mario', '(21)94323-3235'),
-    ('2005-12-03', 'Julia Schmitzz', '(11)94321-4321'),
-    ('2001-08-22', 'Giulia Santos', '(99)32234-2312'),
-    ('2003-06-17', 'Sabrina Soares', '(11)93654-6542'),
-    ('2014-04-03', 'Marina Juliana', '(21)95437-4322');
+    	('1997-05-22', 'Gabriel Eduardo', '(99)94321-3432'),
+    	('1967-04-30', 'Roberto Mario', '(21)94323-3235'),
+    	('2005-12-03', 'Julia Schmitzz', '(11)94321-4321'),
+    	('2001-08-22', 'Giulia Santos', '(99)32234-2312'),
+    	('2003-06-17', 'Sabrina Soares', '(11)93654-6542'),
+    	('2014-04-03', 'Marina Juliana', '(21)95437-4322');
